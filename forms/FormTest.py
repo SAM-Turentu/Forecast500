fc75bfc7ef8491c4d17e149fdff1f5d604865133
SAM-Turentu <SAM-Turentu@outlook.com>
2021-06-17 17:33:21 +0800

    fix:validate call

--- forms/FormTest.py
+++ forms/FormTest.py
@@ -7,13 +7,20 @@
 # Summary: ''
 
 
+import re
+from typing import Any
 import tornado.web
 import tornado.ioloop
 
 
+class FieldName:
+    label = None
+    en_name = None
+
+
 class BaseForm:
 
-    def __init__(self, label: str = None, validators: list = None):
+    def __init__(self, label: str = None, validators: list = None):  # , field_name: FieldName = None
         """
         @author: SAM
         @CreateTime: 2021/6/10 13:19
@@ -27,6 +34,10 @@ class BaseForm:
         self.success = {}
         self.error = {}
         self.validators = validators
+        self.field_name = FieldName()
+        self.field_name.label = label
+        # self.field_name.en_name = None
+
         # self.call_validators()  # 初始化不调用
 
     def check_validate(self):
@@ -48,10 +59,9 @@ class BaseForm:
         if self.validators:
             for validator in self.validators:
                 if callable(validator):
-                    validator(self, self.name_d, self.value)
+                    validator(self)
                 else:
-                    raise Exception('This func do not callable.')
-                # callable(validator(self.name, self.value))
+                    raise Exception('This function cannot be called!')
 
 
 class String(BaseForm):
@@ -65,14 +75,40 @@ class String(BaseForm):
         """
         # self.name_d['en_name'] = en_name
         self.value = value
-        self.call_validators()
+        self.en_name = en_name
+        self.call_validators()  # 先执行附加条件
 
         self.STRING = '^.*$'
         self.TEXT = '^[\s\S]*$'
 
         # 使用正则表达式 验证
+        self.check_str()  # 字符串验证
+
+        # self.success.update({
+        #     self.en_name: self.value
+        # })
 
         return self
+
+    def check_str(self):
+        """
+        @author: SAM
+        @CreateTime: 2021/6/17 16:26
+        @UpdateTime(upf): 2021/6/17 16:26
+        @desc: ''
+        """
+        pattern = re.compile(self.STRING)
+        _match = pattern.match(self.value)
+        if _match:
+            self.success.update({
+                self.en_name: self.value
+            })
+        else:
+            self.flag = False
+            self.error.update({
+                self.en_name: self.value,
+                'label': 'message',
+            })
 
 
 class Float(BaseForm):
@@ -100,18 +136,31 @@ class DateRequired:
         """
         self.message = message
 
-    def __call__(self, cls, name_d, value):
+    def __call__(self, cls):
         """
         @author: SAM
-        @CreateTime: 2021/6/10 13:27
-        @UpdateTime(upf): 2021/6/10 13:27
+        @CreateTime: 2021/6/17 17:22
+        @UpdateTime(upf): 2021/6/17 17:22
         @desc: ''
         """
-        if not value:
+        if not cls.value:
             cls.flag = False
-            message = self.message if self.message else f'{name_d["label"]} 为必填项'
-            cls.error[name_d['en_name']] = message
+            message = self.message if self.message else f'{cls.field_name.label} 为必填项'
+            cls.error[cls.ield_name.en_name] = message
         return cls
+
+    # def __call__(self, cls, name_d, value):
+    #     """
+    #     @author: SAM
+    #     @CreateTime: 2021/6/10 13:27
+    #     @UpdateTime(upf): 2021/6/10 13:27
+    #     @desc: ''
+    #     """
+    #     if not value:
+    #         cls.flag = False
+    #         message = self.message if self.message else f'{name_d["label"]} 为必填项'
+    #         cls.error[name_d['en_name']] = message
+    #     return cls
 
 
 class Length:
@@ -129,21 +178,21 @@ class Length:
         self.max_len = max_len
         self.message = message
 
-    def __call__(self, cls, name_d, value):
+    def __call__(self, cls):
         """
         @author: SAM
         @CreateTime: 2021/6/10 13:28
         @UpdateTime(upf): 2021/6/10 13:28
         @desc: ''
         """
-        if len(value) < self.min_len:
+        if len(cls.value) < self.min_len:
             cls.flag = False
-            message = self.message if self.message else f'{name_d["label"]} 长度不足'
-            cls.error[name_d['en_name']] = message
-        if len(value) > self.max_len:
+            message = self.message if self.message else f'{cls.field_name.label} 长度不足'
+            cls.error[cls.field_name.en_name] = message
+        if len(cls.value) > self.max_len:
             cls.flag = False
-            message = self.message if self.message else f'{name_d["label"]} 长度超出'
-            cls.error[name_d['en_name']] = message
+            message = self.message if self.message else f'{cls.field_name.label} 长度超出'
+            cls.error[cls.field_name.en_name] = message
         return cls
 
 
@@ -162,6 +211,10 @@ class MainForm:
 
         for key, validator in self.__dict__.items():
             value = handler.get_query_argument(key)
+
+            validator.field_name.en_name = key
+            validator.field_name.label = '姓名'
+
             validator.name_d['en_name'] = key
             if type(validator) is String:
                 # 此处调用校验器
@@ -194,6 +247,7 @@ class MainTestForm(MainForm):
         self.name = String(label='姓名', validators=[DateRequired('Hello world!'), Length(1, 10, 'check string length.')])
 
 
+# region Description
 class BaseHandler(tornado.web.RequestHandler):
 
     def __init__(self, application, request, **kwargs):
@@ -204,6 +258,18 @@ class BaseHandler(tornado.web.RequestHandler):
         @desc: ''
         """
         super(BaseHandler, self).__init__(application, request, **kwargs)
+
+    def write_error(self, status_code: int, **kwargs: Any) -> None:
+        """
+        @author: SAM
+        @CreateTime: 2021/6/16 11:06
+        @UpdateTime(upf): 2021/6/16 11:06
+        @desc: ''
+        """
+        exc_cls, exc_instance, trace = kwargs.get('exc_info')
+        if status_code != 200:
+            self.set_status(status_code)
+            self.write(exc_instance.__str__())
 
     def ValidateForm(self, form):
         """
@@ -218,7 +284,7 @@ class BaseHandler(tornado.web.RequestHandler):
             result = {'flag': flag, 'success': success, 'error': error}
             if not flag:
                 self.write({'result': result})
-                raise self.on_finish()
+                # raise Exception(result)
 
 
 class HomeHandler(BaseHandler):
@@ -230,18 +296,22 @@ class HomeHandler(BaseHandler):
         @UpdateTime(upf): 2021/6/10 16:42
         @desc: ''
         """
-        self.ValidateForm(MainTestForm)
-        self.write('test')
-        # obj = MainTestForm()
-        # flag, success, error = obj.check_valid(self)
-        # result = {'flag': flag, 'success': success, 'error': error}
-        # self.write({'result': result})
+        # self.ValidateForm(MainTestForm)
+        obj = MainTestForm()
+        flag, success, error = obj.check_valid(self)
+        result = {'flag': flag, 'success': success, 'error': error}
+        if not flag:
+            return self.write({'result': result})
+        self.write({'result': result})
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", HomeHandler),
     ])
+
+
+# endregion
 
 
 if __name__ == '__main__':
